561fb1833465a544daff3ae2f500ad6c6ced35df
Ivan Yadzvinsky <ivan.yadzvinsky@gmail.com>
2023-08-11 16:32:55 +0300

    update src/types/data.ts

--- src/types/api/data.ts
+++ src/types/api/data.ts
@@ -1,14 +1,13 @@
 type DataRequestQuery = { sources?: string }
 
+// You can create your own source kinds and handle them in src/controllers/data.ts
+type Sources = { [key: string]: { kind: 'local', get: LocalSource, key?: UniqueKey, args: any[] } };
+
 type UniqueKey = 'price' | 'color' | 'name';
 type UnificatorKey = 'id';
+type LocalSource = (...args: any[]) => Promise<LocalDataResponse>
 type LocalData = { id: number } & OneKey<UniqueKey>
 type LocalDataResponse = LocalData[];
-
 type CollectedData = (PromiseSettledResult<{ name: string, key?: UniqueKey, data: LocalDataResponse }> | PromiseRejectedResult)[]
 
 type UnifyFunction = (data: LocalData[], key: UniqueKey, unificator: UnificatorKey) => LocalData[];
-
-type LocalSource = (...args: any[]) => Promise<LocalDataResponse>
-// You can create your own source kinds and handle them in src/controllers/data.ts
-type Sources = { [key: string]: { kind: 'local', get: LocalSource, key?: UniqueKey, args: any[] } };
